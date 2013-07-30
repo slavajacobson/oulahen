b7979c0b6f3cd671479bce13df15877175e234fd
Slava <you@example.com>
2013-07-30 16:13:01 -0700

    void

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,9 +25,9 @@ Oulahen::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-  config.assets.compress = true
+  config.assets.compress = false
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
