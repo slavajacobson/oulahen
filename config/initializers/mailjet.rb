bf78fe7702805c889282734d5b79509b71a63f2b
Slava <you@example.com>
2014-04-30 17:49:27 -0400

    added security

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,6 +1,6 @@
 # initializers/mailjet.rb
 Mailjet.configure do |config|
-  config.api_key = '7d665021ac9e23bb33b999aac83c9b8a'
-  config.secret_key = 'da9d1734bd50bd90837f53fbf232aad1'
+  config.api_key = ENV['mailjet_api_key']
+  config.secret_key = ENV['mailjet_private_key']
   config.default_from = 'vs.jacobson@gmail.com'
 end
